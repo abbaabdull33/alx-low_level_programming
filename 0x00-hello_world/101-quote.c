df426c29f31955362a17f6eaa2c9e0a71832b146
abbaabdull33 <abbabdull33@example.com>
2023-05-27 08:01:39 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 
 /**
  * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
@@ -10,6 +11,5 @@ int main(void)
 
 {
 	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
-	fprintf(stderr, "anything");
 	return (1);
 }
