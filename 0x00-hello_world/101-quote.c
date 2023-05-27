cf0253e0c4030f897f3d19a371ee99f550f517f2
abbaabdull33 <abbabdull33@example.com>
2023-05-27 08:15:28 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <unistd.h>
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
  * followed by a new line, to the standard error.
- * Return: Always 1.
+ * Return: Always 0 (success)
  */
 
 int main(void)
