22da298d5d1ac5fac196caf1745ab1fdef3e3f06
abbaabdull33 <abbabdull33@example.com>
2023-06-02 14:40:09 -0700

    103-infinite_add.c

---
+++ 0x06-pointers_arrays_strings/103-main.c
@@ -0,0 +1,105 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+        char *n = "1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458";
+        char *m = "9034790663470697234682914569346259634958693246597324659762347956349265983465962349569346";
+        char r[100];
+        char r2[10];
+        char r3[11];
+        char *res;
+
+        res = infinite_add(n, m, r, 100);
+        if (res == 0)
+        {
+                printf("Error\n");
+        }
+        else
+        {
+                printf("%s + %s = %s\n", n, m, res);
+        }
+        n = "1234567890";
+        m = "1";
+        res = infinite_add(n, m, r2, 10);
+        if (res == 0)
+        {
+                printf("Error\n");
+        }
+        else
+        {
+                printf("%s + %s = %s\n", n, m, res);
+        }
+        n = "999999999";
+        m = "1";
+        res = infinite_add(n, m, r2, 10);
+        if (res == 0)
+        {
+                printf("Error\n");
+        }
+        else
+        {
+                printf("%s + %s = %s\n", n, m, res);
+        }
+        res = infinite_add(n, m, r3, 11);
+        if (res == 0)
+        {
+                printf("Error\n");
+        }
+        else
+        {
+                printf("%s + %s = %s\n", n, m, res);
+        }
+        return (0);
+}
+julien@ubuntu:~/0x06$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 103-main.c 103-infinite_add.c -o 103-add
+julien@ubuntu:~/0x06$ ./103-add 
+1234567892434574367823574575678477685785645685876876774586734734563456453743756756784458 + 9034790663470697234682914569346259634958693246597324659762347956349265983465962349569346 = 10269358555905271602506489145024737320744338932474201434349082690912722437209719106353804
+Error
+Error
+999999999 + 1 = 1000000000
+julien@ubuntu:~/0x06$ 
+Repo:
+
+GitHub repository: alx-low_level_programming
+Directory: 0x06-pointers_arrays_strings
+File: 103-infinite_add.c
+    
+12. Noise is a buffer, more effective than cubicles or booth walls
+#advanced
+Score: 0.0% (Checks completed: 0.0%)
+Write a function that prints a buffer.
+
+Prototype: void print_buffer(char *b, int size);
+The function must print the content of size bytes of the buffer pointed by b
+The output should print 10 bytes per line
+Each line starts with the position of the first byte of the line in hexadecimal (8 chars), starting with 0
+Each line shows the hexadecimal content (2 chars) of the buffer, 2 bytes at a time, separated by a space
+Each line shows the content of the buffer. If the byte is a printable character, print the letter, if not, print .
+Each line ends with a new line \n
+If size is 0 or less, the output should be a new line only \n
+You are allowed to use the standard library
+The output should look like the following example, and formatted exactly the same way:
+julien@ubuntu:~/0x06$ cat 104-main.c
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char buffer[] = "This is a string!\0And this is the rest of the #buffer :)\1\2\3\4\5\6\7#cisfun\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34\x56#pointersarefun #infernumisfun\n";
+
+    printf("%s\n", buffer);
+    printf("---------------------------------\n");
+    print_buffer(buffer, sizeof(buffer));
+    return (0);
+}
