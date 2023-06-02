33b871faa7ac83aab90f8b8b7acac7f29a335714
abbaabdull33 <abbabdull33@example.com>
2023-06-02 07:05:29 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,7 +1,7 @@
 #ifndef fILE_MAIN
-#define FILE_MAIN
+#define FILE_MAI
 
-int_putchar(char c);
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
