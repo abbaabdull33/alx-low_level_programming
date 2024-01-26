dbe472fb90bb29a51c4e22ce25a6eb5fece189f5
abba abdullahi <abbaabdull33@gmail.com>
2024-01-26 11:13:14 -0800

    abba

---
+++ 0x17-doubly_linked_lists/102-find_palindrome.py
@@ -0,0 +1,17 @@
+def is_palindrome(n):
+    return str(n) == str(n)[::-1]
+
+def find_largest_palindrome():
+    max_palindrome = 0
+    for i in range(100, 1000):
+        for j in range(100, 1000):
+            product = i * j
+            if is_palindrome(product) and product > max_palindrome:
+                max_palindrome = product
+    return max_palindrome
+
+result = find_largest_palindrome()
+
+with open('102-result', 'w') as file:
+    file.write(str(result))
+
